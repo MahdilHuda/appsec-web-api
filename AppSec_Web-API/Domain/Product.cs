517f031656186fb3be14f71cada7037518b3b6e9
oiygiyofiguhj <oiygiyofiguhj@DESKTOP-KAM0BQT>
2026-02-06 10:31:26 +0100

    Add project files.

---
+++ AppSec_Web-API/Domain/Product.cs
@@ -0,0 +1,10 @@
+﻿namespace AppSec_Web_API.Domain
+{
+    public class Product
+    {
+        int Id { get; set; }
+        string Name { get; set; }
+        decimal Price { get; set; }
+
+    }
+}

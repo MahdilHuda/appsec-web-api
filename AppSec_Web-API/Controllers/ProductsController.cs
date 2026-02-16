81bb837b6e26c7e809f86cd10633e3d509493ae6
MahdilHuda <dopzen@proton.me>
2026-02-16 09:47:04 +0100

    Fix: server-generated IDs (remove over-posting)

--- AppSec_Web-API/Controllers/ProductsController.cs
+++ AppSec_Web-API/Controllers/ProductsController.cs
@@ -1,6 +1,7 @@
 ﻿using AppSec_Web_API.Domain;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using AppSec_Web_API.DTO;
 
 namespace AppSec_Web_API.Controllers
 {
@@ -9,6 +10,7 @@ namespace AppSec_Web_API.Controllers
     public class ProductsController : ControllerBase
     {
         private static readonly List<Product> _products = new();
+        private static int _nextId = 1;
 
         [HttpGet]
         public IActionResult GetAll()
@@ -17,8 +19,16 @@ namespace AppSec_Web_API.Controllers
         }
 
         [HttpPost]
-        public IActionResult Create(Product product)
+        public IActionResult Create(CreateProductRequest request)
         {
+            var id = _nextId++;
+            var product = new Product
+            {
+                Id = id,
+                Name = request.Name,
+                Price = request.Price
+            };
+
             _products.Add(product);
             return CreatedAtAction(nameof(GetAll), product);
         }
